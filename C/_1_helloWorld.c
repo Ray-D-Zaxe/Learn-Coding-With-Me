67c3f1ed7c74d710b7cca79fa6060184bb6a0f6e
Ray-D-Zaxe <ray.dreamer400@gmail.com>
2025-01-17 21:37:41 +0530

    C file

---
+++ C/_1_helloWorld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Hello World\n");
+
+    printf("Enter a number: ");
+    int i;
+    scanf("%d", &i);
+    
+    printf("The number is %d\n", i);
+}

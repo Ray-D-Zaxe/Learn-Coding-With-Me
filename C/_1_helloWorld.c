fc4194600cd24621c0f95dcd8a1a3e183e044bf1
Ray-D-Zaxe <ray.dreamer400@gmail.com>
2025-01-21 21:21:33 +0530

    Minor Changes

--- C/_1_helloWorld.c
+++ C/_1_helloWorld.c
@@ -5,8 +5,11 @@ void main()
     printf("Hello World\n");
 
     printf("Enter a number: ");
-    int i;
+    int i, j = 10, k, m;
     scanf("%d", &i);
     
     printf("The number is %d\n", i);
+
+
+    printf("%d, %d, %d\n", i, j, k, m);
 }

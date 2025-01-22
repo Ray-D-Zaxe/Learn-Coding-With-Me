9a8dd3b566c2ccb7863383242b1b0a432cb54e89
Ray-D-Zaxe <ray.dreamer400@gmail.com>
2025-01-22 13:01:39 +0530

    Characters

--- C/_1_helloWorld.c
+++ C/_1_helloWorld.c
@@ -35,9 +35,20 @@ void main()
 
     printf("%%d = %d, %%c = %c, %%c = %c, %%d = %d, %%f = %f\n", 7, 7, 'A', 'A', 'a');
                                                         // Format specifiers can be used to print values directly, using %d, %c, %f etc
-
+    // when an integer is passed in %c, the character corresponding to the ASCII integer value is printed
     
+
+
+    char chr = 'Hello', char_ar[] = "Hello";            // char is a single character and only returns the last character, char_ar is an array of characters
+    printf("chr(%%c) = %c, char_ar(%%s) = %s\n", chr, char_ar);
+    printf("chr(%%s) = %%s, char_ar(%%c) = %c\n", /*ch,*/ char_ar);
+
+
 
     int a = i = j = k = 9;                              // same value can be assigned in this fashion to multiple variables as lon as they are all initilized
     
+    float f1 = 35e3;                                    // e or E is the exponential operator used to denote a number in scientific notation
+    double d1 = 12E4;
+
+    printf("f1 = %f, d1 = %lf\n", f1, d1); 
 }

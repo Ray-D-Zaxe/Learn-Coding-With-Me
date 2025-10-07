2536f3aeb9f32098b69486fb62e51cc36d53c4ed
Ray <ray.dreamer400@gmail.com>
2025-10-07 22:05:02 +0530

    async await future in dart

--- Dart/_10_asyncAwaitFutures.dart
+++ Dart/_10_asyncAwaitFutures.dart
@@ -1,7 +1,31 @@
-void main() {
+void main() async {
 
   // Futures --> like promises in JavaScript
   // can have uncompleted or completed state
 
+  final post = await fetchPost();
+  print(post.title);
+  print(post.userID);
   
+  /*
+  fetchPost().then((p) {
+    print(p.title);
+    print(p.userID);
+  });
+  */
+}
+
+Future<Post> fetchPost() {
+  const delay = Duration(seconds: 3);
+
+  return Future.delayed(delay, (){
+    return Post('my post', 123);
+  });
+}
+
+class Post {
+  String title;
+  int userID;
+
+  Post(this.title, this.userID);
 }

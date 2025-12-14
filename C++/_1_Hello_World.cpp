9ad13df789f4fe3794688e700ea840ba8808ca92
Ray <ray.dreamer400@gmail.com>
2025-12-14 20:24:36 +0530

    _1_Hello_World

---
+++ C++/_1_Hello_World.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // output
+    cout << "Hello World";
+
+    int i = 1;
+    // input
+    cin >> i;
+    cout << i << endl;
+    
+    return 0;
+}
